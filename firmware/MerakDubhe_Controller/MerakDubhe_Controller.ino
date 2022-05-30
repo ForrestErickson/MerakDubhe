d7d194348b511a1408aeb46b71ad6356421f36dd
Forrest Erickson <forresterickson@comcast.net>
2022-05-29 22:45:33 -0400

    Clean up removing dead code. Make a file for motor code but not yet moved it.

--- firmware/MerakDubhe_Controller/MerakDubhe_Controller.ino
+++ firmware/MerakDubhe_Controller/MerakDubhe_Controller.ino
@@ -1,43 +1,114 @@
 /*
- * Unistep2 example: move
- *
- * Simple example of stepper enumeration and movement via move(steps).
- * If steps > 0 clockwise, else counter clockwise. Non-blocking.
- *
- * 30Jan18
- * R Sanchez
- *
- * This code is released to the public domain
- *
- */
+   MerakDubhe_Controller
+   A controler for the failed bench grinder becomes an equatorial mount ("turning sow's ear" into a mount)
+
+   Based on SEEED Studio Stepper Motor Control - one revolution
+   example usesf the <Stepper.h> library
+
+   Forrest Lee Erickson
+   Date: 20220529
+*/
+
+/*
+  Pin assignments
+  A0               Auto / Serial Control  High if automatic advance.
+  A1 - A5          NC
+
+  D0  RX
+  D1  TX
+  D2
+  D3~
+  D4
+  D5~
+  D6~              /Focus    ?ring?
+  D7               /Shutter  ?tip?
+  D8               MOTOR CONTROL
+  D9~              Output Enable Bridge A, M1
+  D10~             Output Enable Bridge B, M2
+  D11~             MOTOR CONTROL
+  D12              MOTOR CONTROL
+  D13              MOTOR CONTROL
+*/
 
 // include the library
-#include <Unistep2.h>
+ #include <Stepper.h>
+
+const int stepsPerNEMA_Revolution = 200;  // Motor Revolution NEMA17 motor Stepper #32A
+const int RA_GEER_to_NEMA_GEER = 20;  //10 Inch over 1/2 inch.
+const int stepsPerRevolution = stepsPerNEMA_Revolution * RA_GEER_to_NEMA_GEER; // Steps per RA gear revolution
 
 // Define some steppers and the pins they will use
-Unistep2 stepperX(2, 3, 4, 5, 4096, 1000); // pins for IN1, IN2, IN3, IN4, steps per rev, step delay(in micros)
-Unistep2 stepperY(6, 7, 8, 9, 4096, 1000);
+// initialize the Stepper object for Right Assention
+const int ENA = 9;  //Pins for H drivers A and B enable
+const int ENB = 10;
+const int IN1 = 8;  // pins for IN1, IN2, IN3, IN4,
+const int IN2 = 11;
+const int IN3 = 12;
+const int IN4 = 13;
+
+Stepper rightAssentionStepper(stepsPerRevolution, IN1, IN2, IN3, IN4);
+
+
+const int HIGH_TIME_LED = 5000;
+const int LOW_TIME_LED = 5000;
+unsigned long lastLEDtime = 0;
+unsigned long nextLEDchange = 100; //time in ms.
+
+
+void enableRA_Stepper() {
+  pinMode(ENA, OUTPUT); //EnA
+  pinMode(ENB, OUTPUT);  //EnB
+  digitalWrite(ENA, HIGH);
+  digitalWrite(ENB, HIGH);
+}//end enableRA_Stepper()
+
+void disableRA_Stepper() {
+  //saves power
+  digitalWrite(ENA, LOW);
+  digitalWrite(ENB, LOW);
+}//end enableRA_Stepper()
+
+
+void wave() {
+  //Motor back and forth.
+  Serial.println("clockwise");
+  rightAssentionStepper.step(stepsPerRevolution / 10);
+  delay(100);
+  Serial.println("counterclockwise");
+  rightAssentionStepper.step(-stepsPerRevolution / 10);
+}//end wave()
+
+bool isMotorCW = true; //
+bool isAdvancing = true; //
+
+//Functions here
+
+
+//end functions
 
 void setup()
 {
-  // Your setup code here
-  // The library initializes the pins for you
-}
+  Serial.begin(112500);
+  Serial.print("System stepsPerRevolution: ");
+  Serial.println(stepsPerRevolution);
+
+  // Enable H drivers A and B
+  enableRA_Stepper();
+  rightAssentionStepper.setSpeed(6);
+  wave();  //Motor back and forth.
+  disableRA_Stepper();
+
+  // Your application setup code here
+  isMotorCW = true; //
+  isAdvancing = true; //
+
+}//end setup()
 
 void loop()
 {
-  // We need to call run() frequently during loop()
-  stepperX.run();
-  stepperY.run();
 
-  // Create random movements for the steppers
-  if ( stepperX.stepsToGo() == 0 ){ // If stepsToGo returns 0 the stepper is not moving
-    stepperX.move(random(-4000,4000));
-  }
-  if ( stepperY.stepsToGo() == 0 ){
-    stepperY.move(random(-4000,4000));
-  }
+  // wave();  //Motor back and forth.
 
   // Other code
 
-}
+}//end loop()
